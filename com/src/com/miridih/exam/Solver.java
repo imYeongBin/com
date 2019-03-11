14cf163f536b4b900c5aced56a07dce1a776202d
im <im@192.168.219.102>
2019-03-11 12:20:16 +0900

    initial Settings:2019-03-11/12:20

---
+++ com/src/com/miridih/exam/Solver.java
@@ -0,0 +1,14 @@
+package com.miridih.exam;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public interface Solver {
+	/*
+	 * in으로부터 입력을 받아 문제를 풀어 out으로 출력한다.
+	 * @param in 테스트 케이스 입력을 받이들이는 InputStream
+	 * @param out 결과 값을 출력할 PrintStream 
+	 */	
+	public void solve(InputStream in, PrintStream out);
+
+}
---
+++ com/src/com/miridih/exam/Stock.java
@@ -0,0 +1,22 @@
+package com.miridih.exam;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class Stock implements Solver{
+	public static void main(String[] args) {
+		new Stock().solve(System.in, System.out);		
+	}
+	
+	/*
+	 * 문제 풀이 메인 메소드   
+	 * @param in 테스트 케이스 입력을 받이들이는 InputStream
+	 * @param out 결과값을 출력할 PrintStream 
+	 */
+	public void solve(InputStream in, PrintStream out) {
+		//
+		// .....
+		// 
+	}
+
+}
