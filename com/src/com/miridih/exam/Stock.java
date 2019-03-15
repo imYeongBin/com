c0b1e3e5560da91fb8fdd7c427b3d394ea6b229c
nainc <nainc@DESKTOP-RMQU72P>
2019-03-15 14:52:34 +0900

    stockTest final:2019-03-14

--- com/src/com/miridih/exam/Stock.java
+++ com/src/com/miridih/exam/Stock.java
@@ -18,24 +18,7 @@ public class Stock implements Solver{
 	 
 		
 	public void solve(InputStream in, PrintStream out) {
-		//예시
-		int testCase = 3;
-		int []days = {3,3,4};
-		int [][]prices= {{5,3,2} , {1,2,100} , {1,3,1,2} };
-		
-		//전체 구매금액 (구매주식1 * 당일 주가)
-		int tot = 0;		
-		// 내부 반복  :  days 만큼 prices[][here]
-		// 외부 반복  :  testCase 만큼 prices[here][]
-		int idx_a=0;
-		for(idx_a=0;idx_a<prices[0].length;idx_a++) {
-			//오늘 예측가 > 내일 예측가  (주가 하락)
-			if(prices[0][idx_a] > prices[0][idx_a+1]) {
-				tot +=prices[0][idx_a];
-				System.out.println("전체금액"+tot);
-			}	
-		}
-		
+
 		
 		
 	}
